6bd0e4acd31bc6037ccdd2a68f36d6c4e4773107
Asairo <wiki908@qq.com>
2014-12-18 15:52:47 +0800

    add code

---
+++ UndoStruct/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UndoStruct
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            refresh();
+        }
+
+        UndoPacker<string> ups = new UndoPacker<string>(2);
+
+        void refresh()
+        {
+            listBox.Items.Clear();
+            btnRedo.IsEnabled = btnUndo.IsEnabled = false;
+            if ((ups.state() & UndoPacker<string>.RedoAvaliable) != 0) btnRedo.IsEnabled = true;
+            if ((ups.state() & UndoPacker<string>.UndoAvaliable) != 0) btnUndo.IsEnabled = true;
+
+            var sl = ups.content();
+            foreach (var s in sl)
+            {
+                listBox.Items.Add(s);
+            }
+        }
+
+        private void btnPush_Click(object sender, RoutedEventArgs e)
+        {
+            ups.push(textBox.Text);
+            refresh();
+        }
+
+        private void btnUndo_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = ups.undo();
+            refresh();
+        }
+
+        private void btnRedo_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = ups.redo();
+            refresh();
+        }
+    }
+}
---
+++ UndoStruct/UndoPacker.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UndoStruct
+{
+    class UndoPacker<Type>
+    {
+        Type[] _chain = null; // 环形列表
+
+        int _volume = 0;
+        int idx(int Index) // 求解实际下标
+        {
+            return Index % _volume;
+        }
+
+        public UndoPacker(int Count)
+        {
+            _idx = _volume = Count; // _volume 初始化后不会使实际下标成为 -1
+            _chain = new Type[Count];
+        }
+
+        int _redo_count = 0;
+        int _undo_count = 0;
+        int _idx = 0; // 虚拟下表
+        bool _undoing = false;
+
+        public void push(Type item)
+        {
+            //MessageBox.Show(idx(_idx++).ToString());
+            if (_undoing) // 多退少补: 指针向前移, undo_count 同进退
+            {
+                ++_idx;
+                if (_undo_count < _volume - 1) ++_undo_count;
+            }
+
+            _chain[idx(_idx++)] = item;
+            if (_undo_count < _volume - 1 && !_undoing) _undo_count++;
+
+            _redo_count = 0;
+            _undoing = false;
+        }
+
+        public Type undo()
+        {
+            _redo_count++;
+            _undo_count--;
+
+            if (!_undoing) // 多退少补: 指针向后移, undo_count 同进退
+                _idx -= 2;
+            else
+                _idx--;
+
+            _undoing = true;
+            return _chain[idx(_idx)];
+        }
+
+        public Type redo()
+        {
+            _redo_count--;
+            if (_undo_count < _volume - 1) _undo_count++;
+            return _chain[idx(++_idx)];
+        }
+
+        public Type[] content()
+        {
+            return _chain;
+        }
+
+        public static int UndoAvaliable = 1;
+        public static int RedoAvaliable = 2;
+        public int state()
+        {
+            int ret = 0;
+
+            if (_redo_count > 0) ret |= RedoAvaliable;
+            if (_undo_count > 0) ret |= UndoAvaliable;
+            if (_undo_count == _idx || _undo_count == _volume - 1) ret &= ~RedoAvaliable;
+            //ret &= ~UndoAvaliable;
+
+            return ret;
+        }
+    }
+}
